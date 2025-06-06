32c9f7e041f216f03aa66abd6518234b7408d359
nfongster <nsfong79@gmail.com>
2025-06-05 22:03:03 -0700

    Add recursive function to copy contents of source folder to destination folder

--- src/main.py
+++ src/main.py
@@ -1,12 +1,39 @@
 from nodes import *
+import os, shutil
+
+
+def __copy_recursive(source, destination):
+    if os.path.isfile(source):
+        print(f"Copying {source} ---> {destination}")
+        shutil.copy(source, destination)
+        return
+    
+    print(f"Recursively copying files in {source} ---> {destination}")
+    if not os.path.exists(destination):
+        os.mkdir(destination)
+    for item in os.listdir(source):
+        __copy_recursive(os.path.join(source, item), os.path.join(destination, item))
+
+
+def copy_static(source, destination):
+    if not os.path.exists(source):
+        raise ValueError(f"Nonexistent source directory: {source}")
+    if not os.path.exists(destination):
+        os.mkdir(destination)
+    
+    print(f"This app will copy the contents of {source} ---> {destination}")
+    print(f"Deleting the following files in {destination}:")
+    for filename in os.listdir(destination):
+        print(f"\t{filename}")
+    
+    shutil.rmtree(destination)
+    os.mkdir(destination)
+    print("Copying files...")
+    __copy_recursive(source, destination)
+
 
 def main():
-    text = "This is some anchor text"
-    text_type = TextType.LINK
-    url = "https://www.boot.dev"
-
-    node = TextNode(text, text_type, url)
-    print(node)
+    copy_static("../static", "../public")
 
 
 if __name__ == '__main__':
